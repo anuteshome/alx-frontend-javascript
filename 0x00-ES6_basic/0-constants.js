5cd068382e3109f97bcb353258000c3123f7625f
anuteshome <anut15821@gmail.com>
2024-03-04 14:52:56 +0300

    Convert line endings to LF

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-	  const  task = 'I prefer const when I can.';
+	  const task = 'I prefer const when I can.'; // Using const instead of var
 	  return task;
 }
 
@@ -8,8 +8,10 @@ export function getLast() {
 }
 
 export function taskNext() {
-	  let combination = 'But sometimes let';
+	  let combination = 'But sometimes let'; // Using let instead of var
 	  combination += getLast();
 
 	  return combination;
+}
+
 
