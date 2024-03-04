ef7fc63046c8b7e263e8616493757e26b7ea4e88
anuteshome <ananyateshome2@gmail.com>
2024-03-04 18:39:29 +0000

    change var by const and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,4 @@
 export function taskFirst() {
-<<<<<<< HEAD
   const task = 'I prefer const when I can.';
   return task;
 }
@@ -9,26 +8,8 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let  combination = 'But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 }
-=======
-	  const task = 'I prefer const when I can.'; // Using const instead of var
-	  return task;
-}
-
-export function getLast() {
-	  return ' is okay';
-}
-
-export function taskNext() {
-	  let combination = 'But sometimes let'; // Using let instead of var
-	  combination += getLast();
-
-	  return combination;
-}
-
-
->>>>>>> 5cd068382e3109f97bcb353258000c3123f7625f
