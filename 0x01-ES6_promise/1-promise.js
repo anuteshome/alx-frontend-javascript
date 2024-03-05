803f8a77930582e7a905ba50aa6928fa3fa52d9d
anuteshome <ananyateshome2@gmail.com>
2024-03-05 16:41:36 +0000

    resolve and reject

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function  getFullResponseFromAPI(success){
+    return new Promise((resolve,reject)=>{
+if(success===true){
+    resolve({
+        status: 200,
+        body: 'Success'
+
+    });
+}else{
+    reject(new error("The fake API is not working currently"));
+}
+
+    });
+}
