dfa153cf8cb3c755bf1ee738e8dad521512175a8
anuteshome <ananyateshome2@gmail.com>
2024-03-11 16:30:41 +0000

    return array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,9 +4,8 @@ export default function getListStudents(){
     return [
  {id :1 ,firstName :"Guillaume",location:"San Francisco"},
  {id :2,firstName:"James",location:"Columbia"},
- {i: 5,firstName:"Serena",location:"San Francisco"},
+ {id :5,firstName:"Serena",location:"San Francisco"},
      ];
  
  
  }
-
